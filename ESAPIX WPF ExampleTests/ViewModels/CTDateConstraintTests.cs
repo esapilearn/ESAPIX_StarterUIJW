1b59963a78275832eb93ff71cc7dd723fd71bb04
Jared Weatherford <jaredhweatherford@yahoo.com>
2024-05-14 11:52:48 -0700

    ct date tested

---
+++ ESAPIX WPF ExampleTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESAPX_StarterUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Constraints;
+
+namespace ESAPX_StarterUI.ViewModels.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void CTDatePassesWhenNewerThan60Days()
+        {
+            var ctDate = DateTime.Now.AddDays(-59);
+
+            var actual = new CTDateConstraint().ConstrainCTDate(ctDate).ResultType;
+
+            var expected = ResultType.PASSED;
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void CTDateFailsWhenNewerThan60Days()
+        {
+            var ctDate = DateTime.Now.AddDays(-61);
+
+            var actual = new CTDateConstraint().ConstrainCTDate(ctDate).ResultType;
+
+            var expected = ResultType.ACTION_LEVEL_2;
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ ESAPIX_StarterUI1/ViewModels/CTDateConstraint.cs
@@ -0,0 +1,39 @@
+﻿using ESAPIX.Constraints;
+using System;
+using VMS.TPS.Common.Model.API;
+
+namespace ESAPX_StarterUI.ViewModels
+{
+    public class CTDateConstraint : IConstraint
+    {
+        public string Name => "CT DATE CONSTRAINT";
+
+        public string FullName => "CT < 60 days old";
+
+        public ConstraintResult CanConstrain(PlanningItem pi)
+        {
+            if(pi.StructureSet.Image!=null) { return new ConstraintResult(this, ResultType.PASSED, ""); }
+            else { return new ConstraintResult(this, ResultType.NOT_APPLICABLE, "Missing CT"); }
+        }
+
+        public ConstraintResult Constrain(PlanningItem pi)
+        {
+            var ctDate = pi.StructureSet.Image.CreationDateTime;
+            return ConstrainCTDate(ctDate.Value);
+        }
+
+        public ConstraintResult ConstrainCTDate(System.DateTime ctDate)
+        {
+            var daysOld = (DateTime.Now - ctDate).TotalDays;
+            var limit = DateTime.Now.AddDays(-60);
+            if (ctDate > limit)
+            {
+                return new ConstraintResult(this, ResultType.PASSED, $"The CT is {daysOld} days old");
+            }
+            else
+            {
+                return new ConstraintResult(this, ResultType.ACTION_LEVEL_3, $"The CT is {daysOld} days old");
+            }
+        }
+    }
+}
